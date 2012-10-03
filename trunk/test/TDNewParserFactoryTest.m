81e143c95c92b1498521668723775de2f63ed885
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2012-10-03 18:54:28 +0000

    new Factory

--- trunk/test/TDNewParserFactoryTest.m
+++ trunk/test/TDNewParserFactoryTest.m
@@ -26,7 +26,7 @@
 
 
 - (void)testSomething {
-    NSString *g = @"foo=bar;bar=Word;";
+    NSString *g = @"foo=bar;bar=baz|bat;baz=Word;bat=Num;";
 
     NSError *err = nil;
     PKParser *p = [_factory parserFromGrammar:g assembler:nil error:&err];
