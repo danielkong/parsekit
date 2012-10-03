b2f41c452f7c7165a13c193feac33ae7981effc4
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2012-10-03 02:50:26 +0000

    custom char classes in regex matcher

--- test/TDTestScaffold.m
+++ test/TDTestScaffold.m
@@ -17,7 +17,7 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
 
-#define RUN_ALL_TEST_CASES 0
+#define RUN_ALL_TEST_CASES 1
 #define SOLO_TEST_CASE @"TDRegexMatcherTest"
 
 @interface SenTestSuite (TDAdditions)
