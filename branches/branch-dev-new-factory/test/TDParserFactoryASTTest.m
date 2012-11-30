08a2779b1d1be55b1d1fd98efac7b5a298ad7a17
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2012-11-30 19:35:25 +0000

    AST redesign

--- branches/branch-dev-new-factory/test/TDParserFactoryASTTest.m
+++ branches/branch-dev-new-factory/test/TDParserFactoryASTTest.m
@@ -513,15 +513,15 @@
 }
 
 
-//- (void)testLiteral5 {
-//    NSString *g = @"@start = ((Word | Number)* | ('$' '%')+) QuotedString;";
-//    
-//    NSError *err = nil;
-//    PKAST *rootNode = [_factory ASTFromGrammar:g simplify:NO error:&err];
-//    
-//    TDNotNil(rootNode);
-//    TDEqualObjects(@"(@start:SEQ (:| (:* (:| :Word :Number)) (:+ (:SEQ :'$' :'%'))) :QuotedString)", [rootNode treeDescription]);
-//    
-//}
+- (void)testLiteral5 {
+    NSString *g = @"@start = ((Word | Number)* | ('$' '%')+) QuotedString;";
+    
+    NSError *err = nil;
+    PKAST *rootNode = [_factory ASTFromGrammar:g simplify:NO error:&err];
+    
+    TDNotNil(rootNode);
+    TDEqualObjects(@"(@start:SEQ (:SEQ (:| (:SEQ (:* (:| (:SEQ :Word) (:SEQ :Number)))) (:SEQ (:+ (:SEQ :'$' :'%')))) :QuotedString))", [rootNode treeDescription]);
+    
+}
 
 @end
