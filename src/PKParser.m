8053980bfc511d5edb2adc052195d73b8af1d520
todd.ditchendorf <todd.ditchendorf@gmail.com>
2013-02-20 02:25:51 +0000

    track feature in grammars

--- src/PKParser.m
+++ src/PKParser.m
@@ -166,41 +166,39 @@
 - (id)parse:(NSString *)s error:(NSError **)outError {
     id result = nil;
     
-    @autoreleasepool {
-        @try {
-            
-            PKTokenizer *t = self.tokenizer;
-            if (!t) {
-                t = [PKTokenizer tokenizer];
-            }
-            t.string = s;
-            PKAssembly *a = [self completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
-            if (a.target) {
-                result = a.target;
-            } else {
-                result = [a pop];
-            }
-            
-            [result retain];
+    @try {
+        
+        PKTokenizer *t = self.tokenizer;
+        if (!t) {
+            t = [PKTokenizer tokenizer];
         }
-        @catch (NSException *ex) {
-            if (outError) {
-                NSMutableDictionary *userInfo = [NSMutableDictionary dictionaryWithDictionary:[ex userInfo]];
-                
-                // get reason
-                NSString *reason = [ex reason];
-                if ([reason length]) [userInfo setObject:reason forKey:NSLocalizedFailureReasonErrorKey];
-                
-                // get domain
-                NSString *exName = [ex name];
-                NSString *domain = exName ? exName : @"PKParseException";
-                
-                // convert to NSError
-                NSError *err = [NSError errorWithDomain:domain code:47 userInfo:[[userInfo copy] autorelease]];
-                *outError = err;
-            } else {
-                [ex raise];
-            }
+        t.string = s;
+        PKAssembly *a = [self completeMatchFor:[PKTokenAssembly assemblyWithTokenizer:t]];
+        if (a.target) {
+            result = a.target;
+        } else {
+            result = [a pop];
+        }
+        
+        [result retain];
+    }
+    @catch (NSException *ex) {
+        if (outError) {
+            NSMutableDictionary *userInfo = [NSMutableDictionary dictionaryWithDictionary:[ex userInfo]];
+            
+            // get reason
+            NSString *reason = [ex reason];
+            if ([reason length]) [userInfo setObject:reason forKey:NSLocalizedFailureReasonErrorKey];
+            
+            // get domain
+            NSString *exName = [ex name];
+            NSString *domain = exName ? exName : @"PKParseException";
+            
+            // convert to NSError
+            NSError *err = [NSError errorWithDomain:domain code:47 userInfo:[[userInfo copy] autorelease]];
+            *outError = err;
+        } else {
+            [ex raise];
         }
     }
 
