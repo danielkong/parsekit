6185bec2a27397d8146bad6416ee96a1701e1630
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-03-14 02:48:48 +0000

    adding new node classes getting factory compiling
    it builds!

---
+++ branches/branch-dev-new-factory-2/test/nodes/PKWhitespaceNode.m
@@ -0,0 +1,21 @@
+//
+//  PKNodeWhitespace.m
+//  ParseKit
+//
+//  Created by Todd Ditchendorf on 10/8/12.
+//
+//
+
+#import "PKWhitespaceNode.h"
+
+@implementation PKWhitespaceNode
+
+- (int)type {
+    return PKNodeTypeWhitespace;
+}
+
+
+- (void)visit:(id <PKNodeVisitor>)v; {
+    [v visitWhitespace:self];
+}
+@end
