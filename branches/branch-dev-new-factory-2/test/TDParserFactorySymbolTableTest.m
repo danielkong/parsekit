1ceef8ffea12576ef15850c4fb7b9461f3a2de5b
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-03-17 19:49:42 +0000

    PKParserFactory symboltable test

--- branches/branch-dev-new-factory-2/test/TDParserFactorySymbolTableTest.m
+++ branches/branch-dev-new-factory-2/test/TDParserFactorySymbolTableTest.m
@@ -45,31 +45,27 @@
 }
 
 
-//- (void)testAlternationAST {
-//    NSString *g = @"@start=foo;foo=Word|Number;";
-//    
-//    NSError *err = nil;
-//    PKSymbolTable *symTab = [_factory symbolTableFromGrammar:g error:&err];
-//    TDNotNil(symTab);
-//    
-//    PKBaseSymbol *start = [symTab resolve:@"@start"];
-//    TDNotNil(start);
-//    TDTrue([start isKindOfClass:[PKVariableSymbol class]]);
-//    
-//    TDNotNil(start.type);
-//    TDTrue([start.type isKindOfClass:[PKBuiltInTypeSymbol class]]);
-//    TDEqualObjects(@"Sequence", start.type.name);
-//    
-//    PKBaseSymbol *foo = [symTab resolve:@"foo"];
-//    TDNotNil(foo);
-//    TDTrue([foo isKindOfClass:[PKVariableSymbol class]]);
-//    
-//    TDNotNil(foo.type);
-//    TDTrue([foo.type isKindOfClass:[PKBuiltInTypeSymbol class]]);
-//    TDEqualObjects(@"Alternation", foo.type.name);
-//}
-//
-//
+- (void)testAlternationAST {
+    NSString *g = @"@start=foo;foo=Word|Number;";
+    
+    NSError *err = nil;
+    NSDictionary *symTab = [_factory symbolTableFromGrammar:g error:&err];
+    TDNotNil(symTab);
+    
+    PKCollectionParser *start = symTab[@"@start"];
+    TDNotNil(start);
+    TDTrue([start isKindOfClass:[PKSequence class]]);
+    
+    PKAlternation *foo = symTab[@"foo"];
+    TDNotNil(foo);
+    TDTrue([foo isKindOfClass:[PKAlternation class]]);
+    
+    TDEquals(start.subparsers[0], foo);
+    TDTrue([foo.subparsers[0] isKindOfClass:[PKWord class]]);
+    TDTrue([foo.subparsers[1] isKindOfClass:[PKNumber class]]);
+}
+
+
 //- (void)testSequenceAST {
 //    NSString *g = @"@start=foo;foo=(Word|Number) Symbol;";
 //    
