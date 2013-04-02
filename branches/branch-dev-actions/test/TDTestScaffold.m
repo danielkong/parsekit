f26d2b7fa862f0513e16ef23d7e378a9772fc1c5
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-04-02 01:13:27 +0000

    alt parser test

--- branches/branch-dev-actions/test/TDTestScaffold.m
+++ branches/branch-dev-actions/test/TDTestScaffold.m
@@ -17,8 +17,8 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
 
-#define RUN_ALL_TEST_CASES 1
-#define SOLO_TEST_CASE @"MultipleParserTest" // TDParserFactoryASTTest TDParserFactorySymbolTableTest TDParserFactoryParserTest TDParseTreeTest TDParserFactoryASTRewriteTest
+#define RUN_ALL_TEST_CASES 0
+#define SOLO_TEST_CASE @"AltParserTest" // TDParserFactoryASTTest TDParserFactorySymbolTableTest TDParserFactoryParserTest TDParseTreeTest TDParserFactoryASTRewriteTest
 
 @interface SenTestSuite (TDAdditions)
 - (void)addSuitesForClassNames:(NSArray *)classNames;
