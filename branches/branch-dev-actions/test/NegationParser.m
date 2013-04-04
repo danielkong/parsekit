62bca15c6340115501d0cac71618973b64e17520
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-04-04 00:04:29 +0000

    negation parser + tests. not working

---
+++ branches/branch-dev-actions/test/NegationParser.m
@@ -0,0 +1,135 @@
+#import "NegationParser.h"
+#import <ParseKit/ParseKit.h>
+#import "PKSRecognitionException.h"
+
+#define LT(i) [self LT:(i)]
+#define LA(i) [self LA:(i)]
+#define LS(i) [self LS:(i)]
+#define LF(i) [self LF:(i)]
+
+#define POP()       [self._assembly pop]
+#define POP_STR()   [self _popString]
+#define POP_TOK()   [self _popToken]
+#define POP_BOOL()  [self _popBool]
+#define POP_INT()   [self _popInteger]
+#define POP_FLOAT() [self _popDouble]
+
+#define PUSH(obj)     [self._assembly push:(id)(obj)]
+#define PUSH_BOOL(yn) [self _pushBool:(BOOL)(yn)]
+#define PUSH_INT(i)   [self _pushInteger:(NSInteger)(i)]
+#define PUSH_FLOAT(f) [self _pushDouble:(double)(f)]
+
+#define EQ(a, b) [(a) isEqual:(b)]
+#define NE(a, b) (![(a) isEqual:(b)])
+#define EQ_IGNORE_CASE(a, b) (NSOrderedSame == [(a) compare:(b)])
+
+#define ABOVE(fence) [self._assembly objectsAbove:(fence)]
+
+#define LOG(obj) do { NSLog(@"%@", (obj)); } while (0);
+#define PRINT(str) do { printf("%s\n", (str)); } while (0);
+
+@interface PKSParser ()
+@property (nonatomic, retain) PKAssembly *_assembly;
+@end
+
+@interface NegationParser ()
+@property (nonatomic, retain) NSDictionary *_tokenKindTab;
+@end
+
+@implementation NegationParser
+
+- (id)init {
+	self = [super init];
+	if (self) {
+		self._tokenKindTab = @{
+           @"foo" : @(TOKEN_KIND_FOO),
+           @"bar" : @(TOKEN_KIND_BAR),
+        };
+	}
+	return self;
+}
+
+- (void)dealloc {
+	self._tokenKindTab = nil;
+	[super dealloc];
+}
+
+- (NSInteger)tokenKindForString:(NSString *)s {
+    NSInteger x = TOKEN_KIND_BUILTIN_INVALID;
+
+    id obj = _tokenKindTab[s];
+    if (obj) {
+        x = [obj integerValue];
+    }
+    
+    return x;
+}
+
+- (void)_start {
+    
+    @try {
+        [self s]; 
+    }
+    @catch (PKSRecognitionException *ex) {
+        @throw ex;
+    }
+
+    [self fireAssemblerSelector:@selector(parser:didMatch_start:)];
+}
+
+- (void)s {
+    
+    @try {
+        if ((LA(1) == TOKEN_KIND_FOO) && ([self speculate:^{ [self expr]; }])) {
+            [self expr]; 
+        }
+        [self foo]; 
+        [self bar]; 
+    }
+    @catch (PKSRecognitionException *ex) {
+        @throw ex;
+    }
+
+    [self fireAssemblerSelector:@selector(parser:didMatchS:)];
+}
+
+- (void)expr {
+    
+    @try {
+        [self foo]; 
+        [self bar]; 
+        [self bar]; 
+    }
+    @catch (PKSRecognitionException *ex) {
+        @throw ex;
+    }
+
+    [self fireAssemblerSelector:@selector(parser:didMatchExpr:)];
+}
+
+- (void)foo {
+    
+    @try {
+        [self match:TOKEN_KIND_FOO]; 
+    }
+    @catch (PKSRecognitionException *ex) {
+        @throw ex;
+    }
+
+    [self fireAssemblerSelector:@selector(parser:didMatchFoo:)];
+}
+
+- (void)bar {
+    
+    @try {
+        [self match:TOKEN_KIND_BAR]; 
+    }
+    @catch (PKSRecognitionException *ex) {
+        @throw ex;
+    }
+
+    [self fireAssemblerSelector:@selector(parser:didMatchBar:)];
+}
+
+@synthesize _tokenKindTab = _tokenKindTab;
+@end
