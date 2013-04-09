256f3526007e7685d68d400ae6b5681720318f62
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-04-09 20:20:11 +0000

    dot question test

---
+++ branches/branch-dev-self-hosting/test/DotQuestionParser.h
@@ -0,0 +1,9 @@
+#import "PKSParser.h"
+enum {
+    TOKEN_KIND_A = 14,
+};
+
+@interface DotQuestionParser : PKSParser
+
+@end
+
---
+++ branches/branch-dev-self-hosting/test/DotQuestionParser.m
@@ -0,0 +1,79 @@
+#import "DotQuestionParser.h"
+#import <ParseKit/ParseKit.h>
+#import "PKSRecognitionException.h"
+
+#define LT(i) [self LT:(i)]
+#define LA(i) [self LA:(i)]
+#define LS(i) [self LS:(i)]
+#define LF(i) [self LF:(i)]
+
+#define POP()       [self._assembly pop]
+#define POP_STR()   [self _popString]
+#define POP_TOK()   [self _popToken]
+#define POP_BOOL()  [self _popBool]
+#define POP_INT()   [self _popInteger]
+#define POP_FLOAT() [self _popDouble]
+
+#define PUSH(obj)     [self._assembly push:(id)(obj)]
+#define PUSH_BOOL(yn) [self _pushBool:(BOOL)(yn)]
+#define PUSH_INT(i)   [self _pushInteger:(NSInteger)(i)]
+#define PUSH_FLOAT(f) [self _pushDouble:(double)(f)]
+
+#define EQ(a, b) [(a) isEqual:(b)]
+#define NE(a, b) (![(a) isEqual:(b)])
+#define EQ_IGNORE_CASE(a, b) (NSOrderedSame == [(a) compare:(b)])
+
+#define ABOVE(fence) [self._assembly objectsAbove:(fence)]
+
+#define LOG(obj) do { NSLog(@"%@", (obj)); } while (0);
+#define PRINT(str) do { printf("%s\n", (str)); } while (0);
+
+@interface PKSParser ()
+@property (nonatomic, retain) PKAssembly *_assembly;
+@property (nonatomic, retain) NSMutableDictionary *_tokenKindTab;
+
+- (BOOL)_popBool;
+- (NSInteger)_popInteger;
+- (double)_popDouble;
+- (PKToken *)_popToken;
+- (NSString *)_popString;
+
+- (void)_pushBool:(BOOL)yn;
+- (void)_pushInteger:(NSInteger)i;
+- (void)_pushDouble:(double)d;
+@end
+
+@interface DotQuestionParser ()
+@end
+
+@implementation DotQuestionParser
+
+- (id)init {
+	self = [super init];
+	if (self) {
+        self._tokenKindTab[@"a"] = @(TOKEN_KIND_A);
+
+    }
+	return self;
+}
+
+
+- (void)_start {
+    
+    [self a]; 
+    if ([self predicts:TOKEN_KIND_BUILTIN_ANY]) {
+        [self Any]; 
+    }
+    [self a]; 
+
+    [self fireAssemblerSelector:@selector(parser:didMatch_start:)];
+}
+
+- (void)a {
+    
+    [self match:TOKEN_KIND_A]; 
+
+    [self fireAssemblerSelector:@selector(parser:didMatchA:)];
+}
+
+@end
