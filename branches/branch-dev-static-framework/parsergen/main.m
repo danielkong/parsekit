2e25ca122b0a6a1ebf9152d6e00029123f8db569
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-04-15 18:15:39 +0000

    creating static framework and cli

---
+++ branches/branch-dev-static-framework/parsergen/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  parsergen
+//
+//  Created by Todd Ditchendorf on 4/15/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
