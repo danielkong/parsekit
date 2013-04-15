62bc4d284de6fa866546d1b248e2d1d9aadd7fa9
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-04-15 18:15:04 +0000

    create   branch-dev-static-framework branch

---
+++ branches/branch-dev-static-framework/test/LabelEBNFParser.h
@@ -0,0 +1,11 @@
+#import "PKSParser.h"
+enum {
+    TOKEN_KIND_EQUALS = 14,
+    TOKEN_KIND_RETURN,
+    TOKEN_KIND_COLON,
+};
+
+@interface LabelEBNFParser : PKSParser
+
+@end
+
---
+++ branches/branch-dev-static-framework/test/MultipleParser.h
@@ -0,0 +1,10 @@
+#import "PKSParser.h"
+enum {
+    TOKEN_KIND_A = 14,
+    TOKEN_KIND_B,
+};
+
+@interface MultipleParser : PKSParser
+
+@end
+
