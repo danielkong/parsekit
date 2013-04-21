ba980751d6856be4ac66b8b43b53ae681f4dd017
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-04-21 23:33:18 +0000

    json parser

---
+++ test/JSONParserTest.m
@@ -0,0 +1,92 @@
+
+//
+//  JSONParserTest.m
+//  JSON
+//
+//  Created by Todd Ditchendorf on 3/27/13.
+//
+//
+
+#import "JSONParserTest.h"
+#import "PKParserFactory.h"
+#import "PKSParserGenVisitor.h"
+#import "PKRootNode.h"
+#import "JSONParser.h"
+
+@interface JSONParserTest ()
+@property (nonatomic, retain) PKParserFactory *factory;
+@property (nonatomic, retain) PKRootNode *root;
+@property (nonatomic, retain) PKSParserGenVisitor *visitor;
+@property (nonatomic, retain) JSONParser *parser;
+@end
+
+@implementation JSONParserTest
+
+- (void)setUp {
+    self.factory = [PKParserFactory factory];
+    _factory.collectTokenKinds = YES;
+
+    NSError *err = nil;
+    NSString *path = [[NSBundle bundleForClass:[self class]] pathForResource:@"json_with_comments" ofType:@"grammar"];
+    NSString *g = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&err];
+    
+    err = nil;
+    self.root = (id)[_factory ASTFromGrammar:g error:&err];
+    _root.grammarName = @"JSON";
+    
+    self.visitor = [[[PKSParserGenVisitor alloc] init] autorelease];
+    _visitor.assemblerSettingBehavior = PKParserFactoryAssemblerSettingBehaviorOnTerminals;
+    [_root visit:_visitor];
+    
+    self.parser = [[[JSONParser alloc] init] autorelease];
+
+#if TD_EMIT
+    path = [@"~/work/parsekit/trunk/test/JSONParser.h" stringByExpandingTildeInPath];
+    err = nil;
+    if (![_visitor.interfaceOutputString writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:&err]) {
+        NSLog(@"%@", err);
+    }
+
+    path = [@"~/work/parsekit/trunk/test/JSONParser.m" stringByExpandingTildeInPath];
+    err = nil;
+    if (![_visitor.implementationOutputString writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:&err]) {
+        NSLog(@"%@", err);
+    }
+#endif
+}
+
+- (void)tearDown {
+    self.factory = nil;
+}
+
+- (void)testPHi {
+    NSError *err = nil;
+    PKAssembly *res = nil;
+    NSString *input = nil;
+
+    input = @"{'foo':'bar'}";
+    res = [_parser parseString:input assembler:nil error:&err];
+    TDEqualObjects(@"[{, 'foo', 'bar']{/'foo'/:/'bar'/}^", [res description]);
+    
+    input = @"{'foo':{}}";
+    res = [_parser parseString:input assembler:nil error:&err];
+    TDEqualObjects(@"[{, 'foo', {]{/'foo'/:/{/}/}^", [res description]);
+    
+    input = @"{'foo':{'bar':[]}}";
+    res = [_parser parseString:input assembler:nil error:&err];
+    TDEqualObjects(@"[{, 'foo', {, 'bar', []{/'foo'/:/{/'bar'/:/[/]/}/}^", [res description]);
+    
+    input = @"['foo', true, null]";
+    res = [_parser parseString:input assembler:nil error:&err];
+    TDEqualObjects(@"[[, 'foo'][/'foo'/,/true/,/null/]^", [res description]);
+    
+    input = @"[[]]";
+    res = [_parser parseString:input assembler:nil error:&err];
+    TDEqualObjects(@"[[, [][/[/]/]^", [res description]);
+    
+    input = @"[[[1]]]";
+    res = [_parser parseString:input assembler:nil error:&err];
+    TDEqualObjects(@"[[, [, [, 1][/[/[/1/]/]/]^", [res description]);
+}
+
+@end
