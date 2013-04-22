cb9e4228ce9df97a23605ff6e23990d00751ca44
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-04-22 23:45:28 +0000

    recovery resync set

---
+++ test/RecoveryResyncSetTest.m
@@ -0,0 +1,53 @@
+//
+//  RecoveryResyncSetTest.m
+//
+//  Created by Todd Ditchendorf on 3/27/13.
+//
+//
+
+#import "RecoveryResyncSetTest.h"
+#import "PKParserFactory.h"
+#import "PKSParserGenVisitor.h"
+#import "PKRootNode.h"
+#import "ElementAssignParser.h"
+
+@interface RecoveryResyncSetTest ()
+@property (nonatomic, retain) PKParserFactory *factory;
+@property (nonatomic, retain) PKRootNode *root;
+@property (nonatomic, retain) PKSParserGenVisitor *visitor;
+@property (nonatomic, retain) ElementAssignParser *parser;
+@end
+
+@implementation RecoveryResyncSetTest
+
+- (void)setUp {
+    self.parser = [[[ElementAssignParser alloc] init] autorelease];
+}
+
+- (void)tearDown {
+    self.factory = nil;
+}
+
+- (void)testCorrectExpr {
+    NSError *err = nil;
+    PKAssembly *res = nil;
+    NSString *input = nil;
+    
+    input = @"[3];";
+    res = [_parser parseString:input assembler:nil error:&err];
+    TDEqualObjects(@"[[, 3, ;][/3/]/;^", [res description]);
+}
+
+- (void)testMissingElement {
+    NSError *err = nil;
+    PKAssembly *res = nil;
+    NSString *input = nil;
+    
+    _parser.enableAutomaticErrorRecovery = YES;
+    
+    input = @"[];";
+    res = [_parser parseString:input assembler:nil error:&err];
+    TDEqualObjects(@"[[][/]/;^", [res description]);
+}
+
+@end
