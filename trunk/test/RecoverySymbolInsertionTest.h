9fc633ce02e3d2bc1101ba67acb09cde9478ef30
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-04-22 18:41:45 +0000

    recovery by symbol insertion

---
+++ trunk/test/RecoverySymbolInsertionTest.h
@@ -0,0 +1,13 @@
+//
+//  RecoverySymbolInsertionTest.h
+//  ParseKit
+//
+//  Created by Todd Ditchendorf on 3/27/13.
+//
+//
+
+#import "TDTestScaffold.h"
+
+@interface RecoverySymbolInsertionTest : SenTestCase
+
+@end
---
+++ trunk/test/RecoverySymbolInsertionTest.m
@@ -0,0 +1,68 @@
+
+//
+//  RecoverySymbolInsertionTest.m
+//  JSON
+//
+//  Created by Todd Ditchendorf on 3/27/13.
+//
+//
+
+#import "RecoverySymbolInsertionTest.h"
+#import "PKParserFactory.h"
+#import "PKSParserGenVisitor.h"
+#import "PKRootNode.h"
+#import "ElementAssignParser.h"
+
+@interface RecoverySymbolInsertionTest ()
+@property (nonatomic, retain) PKParserFactory *factory;
+@property (nonatomic, retain) PKRootNode *root;
+@property (nonatomic, retain) PKSParserGenVisitor *visitor;
+@property (nonatomic, retain) ElementAssignParser *parser;
+@end
+
+@implementation RecoverySymbolInsertionTest
+
+- (void)setUp {
+    self.parser = [[[ElementAssignParser alloc] init] autorelease];
+}
+
+- (void)tearDown {
+    self.factory = nil;
+}
+
+- (void)testCorrectExpr {
+    NSError *err = nil;
+    PKAssembly *res = nil;
+    NSString *input = nil;
+    
+    input = @"[3];";
+    res = [_parser parseString:input assembler:nil error:&err];
+    TDEqualObjects(@"[[, 3, ;][/3/]/;^", [res description]);
+}
+
+- (void)testMissingBracket {
+    NSError *err = nil;
+    PKAssembly *res = nil;
+    NSString *input = nil;
+    
+    _parser.enableAutomaticErrorRecovery = NO;
+    
+    input = @"[3;";
+    res = [_parser parseString:input assembler:nil error:&err];
+    TDNotNil(err);
+    TDNil(res);
+}
+
+- (void)testMissingBracketWithRecovery {
+    NSError *err = nil;
+    PKAssembly *res = nil;
+    NSString *input = nil;
+    
+    _parser.enableAutomaticErrorRecovery = YES;
+    
+    input = @"[3;";
+    res = [_parser parseString:input assembler:nil error:&err];
+    TDEqualObjects(@"[[, 3, ;][/3/;^", [res description]);
+}
+
+@end
