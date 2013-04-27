73987114da622a89fb17be39310dbf32e5622f37
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-04-27 00:53:54 +0000

    javascript tests / error recovery

--- branches/branch-dev-okuda-int/test/JavaScriptParserTest.m
+++ branches/branch-dev-okuda-int/test/JavaScriptParserTest.m
@@ -20,7 +20,9 @@
 @property (nonatomic, retain) JavaScriptParser *parser;
 @end
 
-@implementation JavaScriptParserTest
+@implementation JavaScriptParserTest {
+    BOOL flag;
+}
 
 - (void)setUp {
     self.factory = [PKParserFactory factory];
@@ -62,37 +64,44 @@
     self.factory = nil;
 }
 
+
+- (void)parser:(PKSParser *)p didMatchVar:(PKAssembly *)a {
+    NSLog(@"%s %@", __PRETTY_FUNCTION__, a);
+    flag = YES;
+}
 - (void)testVarFooEqBar {
     _parser.enableAutomaticErrorRecovery = YES;
     
     NSError *err = nil;
-    PKAssembly *res = [_parser parseString:@"var foo = 'bar';" assembler:nil error:&err];
+    flag = NO;
+    PKAssembly *res = [_parser parseString:@"var foo = 'bar';" assembler:self error:&err];
     TDEqualObjects(@"[var, foo, =, 'bar', ;]var/foo/=/'bar'/;^", [res description]);
+    TDEquals(YES, flag);
 }
 
-- (void)testDocWriteNewDate {
-    NSError *err = nil;
-    PKAssembly *res = [_parser parseString:@"document.write(new Date().toUTCString());" assembler:nil error:&err];
-    TDEqualObjects(@"[document, ., write, (, new, Date, (, ), ., toUTCString, (, ), ), ;]document/./write/(/new/Date/(/)/./toUTCString/(/)/)/;^", [res description]);
-}
-
-- (void)testDocWriteNewDateWithParen {
-    NSError *err = nil;
-    PKAssembly *res = [_parser parseString:@"document.write((new Date()).toUTCString());" assembler:nil error:&err];
-    TDEqualObjects(@"[document, ., write, (, (, new, Date, (, ), ), ., toUTCString, (, ), ), ;]document/./write/(/(/new/Date/(/)/)/./toUTCString/(/)/)/;^", [res description]);
-}
-
-- (void)testDocWriteDate {
-    NSError *err = nil;
-    PKAssembly *res = [_parser parseString:@"document.write(foo.toUTCString());" assembler:nil error:&err];
-    TDEqualObjects(@"[document, ., write, (, foo, ., toUTCString, (, ), ), ;]document/./write/(/foo/./toUTCString/(/)/)/;^", [res description]);
-}
-
-- (void)testGmailUserscriptShort {
-    NSError *err = nil;
-    PKAssembly *res = [_parser parseString:@"window.fluid.dockBadge = ''; setTimeout(updateDockBadge, 1000); setTimeout(updateDockBadge, 3000); setInterval(updateDockBadge, 5000);" assembler:nil error:&err];
-    TDEqualObjects(@"[window, ., fluid, ., dockBadge, =, '', ;, setTimeout, (, updateDockBadge, ,, 1000, ), ;, setTimeout, (, updateDockBadge, ,, 3000, ), ;, setInterval, (, updateDockBadge, ,, 5000, ), ;]window/./fluid/./dockBadge/=/''/;/setTimeout/(/updateDockBadge/,/1000/)/;/setTimeout/(/updateDockBadge/,/3000/)/;/setInterval/(/updateDockBadge/,/5000/)/;^", [res description]);
-}
+//- (void)testDocWriteNewDate {
+//    NSError *err = nil;
+//    PKAssembly *res = [_parser parseString:@"document.write(new Date().toUTCString());" assembler:nil error:&err];
+//    TDEqualObjects(@"[document, ., write, (, new, Date, (, ), ., toUTCString, (, ), ), ;]document/./write/(/new/Date/(/)/./toUTCString/(/)/)/;^", [res description]);
+//}
+//
+//- (void)testDocWriteNewDateWithParen {
+//    NSError *err = nil;
+//    PKAssembly *res = [_parser parseString:@"document.write((new Date()).toUTCString());" assembler:nil error:&err];
+//    TDEqualObjects(@"[document, ., write, (, (, new, Date, (, ), ), ., toUTCString, (, ), ), ;]document/./write/(/(/new/Date/(/)/)/./toUTCString/(/)/)/;^", [res description]);
+//}
+//
+//- (void)testDocWriteDate {
+//    NSError *err = nil;
+//    PKAssembly *res = [_parser parseString:@"document.write(foo.toUTCString());" assembler:nil error:&err];
+//    TDEqualObjects(@"[document, ., write, (, foo, ., toUTCString, (, ), ), ;]document/./write/(/foo/./toUTCString/(/)/)/;^", [res description]);
+//}
+//
+//- (void)testGmailUserscriptShort {
+//    NSError *err = nil;
+//    PKAssembly *res = [_parser parseString:@"window.fluid.dockBadge = ''; setTimeout(updateDockBadge, 1000); setTimeout(updateDockBadge, 3000); setInterval(updateDockBadge, 5000);" assembler:nil error:&err];
+//    TDEqualObjects(@"[window, ., fluid, ., dockBadge, =, '', ;, setTimeout, (, updateDockBadge, ,, 1000, ), ;, setTimeout, (, updateDockBadge, ,, 3000, ), ;, setInterval, (, updateDockBadge, ,, 5000, ), ;]window/./fluid/./dockBadge/=/''/;/setTimeout/(/updateDockBadge/,/1000/)/;/setTimeout/(/updateDockBadge/,/3000/)/;/setInterval/(/updateDockBadge/,/5000/)/;^", [res description]);
+//}
 
 
 
