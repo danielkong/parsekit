8d8276dea0ecf2d187e0165e60037439c5465dfc
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-04-28 18:54:42 +0000

    remove okudakit

--- branches/branch-dev-okuda-remove/okudakit/app/example.js
+++
@@ -1,9 +0,0 @@
-window.fluid.dockBadge = '';
-setTimeout(updateDockBadge, 1000);
-setTimeout(updateDockBadge, 3000);
-setInterval(updateDockBadge, 5000);
-
-function updateDockBadge() {
-    
-	window.fluid.dockBadge = newBadge;
-}
