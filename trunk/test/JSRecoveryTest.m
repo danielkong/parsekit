dfe83d8646d9ab12c725feb0817066ae4fc44619
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-05-14 19:12:45 +0000

    recovery tests
    -didFailToMatch: seems to work

--- trunk/test/JSRecoveryTest.m
+++ trunk/test/JSRecoveryTest.m
@@ -35,6 +35,11 @@
 - (void)parser:(PKSParser *)p didMatchSemi:(PKAssembly *)a {}
 - (void)parser:(PKSParser *)p didMatchVariablesOrExprStmt:(PKAssembly *)a {}
 - (void)parser:(PKSParser *)p didMatchStmt:(PKAssembly *)a {}
+- (void)parser:(PKSParser *)p didMatchFunction:(PKAssembly *)a {}
+- (void)parser:(PKSParser *)p didMatchOpenParen:(PKAssembly *)a {}
+- (void)parser:(PKSParser *)p didMatchCloseParen:(PKAssembly *)a {}
+- (void)parser:(PKSParser *)p didMatchParamListOpt:(PKAssembly *)a {}
+- (void)parser:(PKSParser *)p didMatchFunc:(PKAssembly *)a {}
 - (void)parser:(PKSParser *)p didMatchElement:(PKAssembly *)a {}
 - (void)parser:(PKSParser *)p didMatchProgram:(PKAssembly *)a {}
 
@@ -76,9 +81,6 @@
     PKAssembly *res = nil;
     NSString *input = nil;
     
-//    [[_mock expect] parser:_parser didMatchVar:OCMOCK_ANY];
-//    [[_mock expect] parser:_parser didMatchIdentifier:OCMOCK_ANY];
-
     [[[_mock stub] andDo:^(NSInvocation *invoc) {
         PKAssembly *a = nil;
         [invoc getArgument:&a atIndex:3];
@@ -98,9 +100,7 @@
           TDNotNil(a);
           TDEqualObjects(@"[]var/foo^", [a description]);
     }] parser:_parser didMatchProgram:OCMOCK_ANY];
-    
-    //[[_mock expect] parser:_parser didMatchProgram:OCMOCK_ANY];
-    
+        
     input = @"var foo";
     res = [_parser parseString:input assembler:_mock error:&err];
     TDEqualObjects(@"[]var/foo^", [res description]);
@@ -113,11 +113,10 @@
     PKAssembly *res = nil;
     NSString *input = nil;
     
-//    [[_mock expect] parser:_parser didMatchVar:OCMOCK_ANY];
     [[[_mock stub] andDo:^(NSInvocation *invoc) {
         PKAssembly *a = nil;
         [invoc getArgument:&a atIndex:3];
-        NSLog(@"%@", a);
+        //NSLog(@"%@", a);
         
         TDNotNil(a);
         TDEqualObjects(@"[1, -]1/-^", [a description]);
@@ -138,4 +137,63 @@
     VERIFY();
 }
 
+- (void)testBorkedFunc1 {
+    NSError *err = nil;
+    PKAssembly *res = nil;
+    NSString *input = nil;
+    
+    [[[_mock stub] andDo:^(NSInvocation *invoc) {
+        PKAssembly *a = nil;
+        [invoc getArgument:&a atIndex:3];
+        //NSLog(@"%@", a);
+        
+        TDNotNil(a);
+        TDEqualObjects(@"[function, foo, (, ), {, v]function/foo/(/)/{/v^", [a description]);
+        
+        [a pop]; // `v`
+    }] parser:_parser didFailToMatch:OCMOCK_ANY];
+    [[_mock expect] parser:_parser didMatchFunction:OCMOCK_ANY];
+    [[_mock expect] parser:_parser didMatchIdentifier:OCMOCK_ANY];
+    [[_mock expect] parser:_parser didMatchOpenParen:OCMOCK_ANY];
+    [[_mock expect] parser:_parser didMatchCloseParen:OCMOCK_ANY];
+    [[_mock expect] parser:_parser didMatchParamListOpt:OCMOCK_ANY];
+    [[_mock expect] parser:_parser didMatchFunc:OCMOCK_ANY];
+    [[_mock expect] parser:_parser didMatchElement:OCMOCK_ANY];
+    [[_mock expect] parser:_parser didMatchProgram:OCMOCK_ANY];
+    
+    input = @"function foo(){v}";
+    res = [_parser parseString:input assembler:_mock error:&err];
+    TDEqualObjects(@"[function, foo, (, ), {, }]function/foo/(/)/{/v/}^", [res description]);
+    
+    VERIFY();
+}
+
+//- (void)testBorkedFunc {
+//    NSError *err = nil;
+//    PKAssembly *res = nil;
+//    NSString *input = nil;
+//    
+//    [[[_mock stub] andDo:^(NSInvocation *invoc) {
+//        PKAssembly *a = nil;
+//        [invoc getArgument:&a atIndex:3];
+//        NSLog(@"%@", a);
+//        
+//        TDNotNil(a);
+//        TDEqualObjects(@"[function, foo, (, ), {, v]function/foo/(/)/{/v^", [a description]);
+//        
+//        [a pop]; // `v`
+//    }] parser:_parser didFailToMatch:OCMOCK_ANY];
+//    [[_mock expect] parser:_parser didMatchSemi:OCMOCK_ANY];
+//    [[_mock expect] parser:_parser didMatchStmt:OCMOCK_ANY];
+//    [[_mock expect] parser:_parser didMatchSemi:OCMOCK_ANY];
+//    [[_mock expect] parser:_parser didMatchStmt:OCMOCK_ANY];
+//    [[_mock expect] parser:_parser didMatchProgram:OCMOCK_ANY];
+//    
+//    input = @"function foo() {\n\tv\n}";
+//    res = [_parser parseString:input assembler:_mock error:&err];
+//    TDEqualObjects(@"[function, foo, (, ), {, }]function/foo/(/)/{/v/}^", [res description]);
+//    
+//    VERIFY();
+//}
+//
 @end
