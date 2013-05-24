7f2a96fab1481c0a6b95ca4d9928e01ffa24bf8f
todd.ditchendorf <todd.ditchendorf@47a69902-6c80-45e3-bb3b-c8207f6fa097>
2013-05-24 04:13:37 +0000

    incrementalRuleName

--- src/PKSParser.m
+++ src/PKSParser.m
@@ -40,6 +40,7 @@
 @property (nonatomic, retain) NSMutableArray *_tokenKindNameTab;
 @property (nonatomic, retain) NSCountedSet *_resyncSet;
 @property (nonatomic, retain) NSString *_startRuleName;
+@property (nonatomic, retain) NSString *_incrementalRuleName;
 
 - (NSInteger)tokenKindForString:(NSString *)str;
 - (NSString *)stringForTokenKind:(NSInteger)tokenKind;
@@ -121,6 +122,7 @@
     self._tokenKindNameTab = nil;
     self._resyncSet = nil;
     self._startRuleName = nil;
+    self._incrementalRuleName = nil;
     [super dealloc];
 }
 
@@ -819,4 +821,5 @@
 @synthesize _tokenKindTab = _tokenKindTab;
 @synthesize _resyncSet = _resyncSet;
 @synthesize _startRuleName = _startRuleName;
+@synthesize _incrementalRuleName = _incrementalRuleName;
 @end
